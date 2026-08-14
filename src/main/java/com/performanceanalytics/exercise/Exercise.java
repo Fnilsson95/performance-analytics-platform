1513bc6d86fbefff849de7f3868e02f6b13916b2
fnilsson <fnilsson95@hotmail.com>
2026-08-14 12:28:39 +0200

    Update with explicit commenting

--- src/main/java/com/performanceanalytics/exercise/Exercise.java
+++ src/main/java/com/performanceanalytics/exercise/Exercise.java
@@ -6,10 +6,11 @@ import java.util.Objects;
 
 
 @Entity
-@Table(name = "exercise")
+@Table(name = "exercise") // Explicitly name the table and not from the class name
 public class Exercise {
 
-    @Id
+    @Id // PK
+    // Generated id from the DB via BIGSERIAL sequence from SQL table
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -22,6 +23,7 @@ public class Exercise {
     @Column(nullable = false, length = 50)
     private String equipment;
 
+    // Stores the enums NAME ("BEGINNER") in the DB
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
     private Difficulty difficulty;
@@ -30,22 +32,28 @@ public class Exercise {
     @Column(nullable = false, length = 20)
     private ExerciseType type;
 
+    // columnDefinition overrides Hibernates default type guess, forcing unbounded TEXT instead of length-capped VARCHAR
     @Column(columnDefinition = "text")
     private String instructions;
 
+    // updatable = false so createdAt can't be overwritten later
+    // Instant = a moment in absolute time, one specific moment
     @Column(name = "created_at", nullable = false, updatable = false)
     private Instant createdAt;
+
 
     @Column(name = "updated_at", nullable = false)
     private Instant updatedAt;
 
 
-    // Protected empty constructor
+    // Protected No-args constructor as JPA/Hibernate requires one to exist, because it builds
+    // objects via reflection when loading rows from the DB. Protected keeps random application code form calling it.
+    // Only the framework and subclasses can use it.
     protected Exercise() {
         // JPA only
     }
 
-    // Constructor
+    // Full-args Constructor
     public Exercise(String name, String muscleGroup, String equipment, Difficulty difficulty,
                     ExerciseType type, String instructions) {
         this.name = name;
@@ -56,6 +64,7 @@ public class Exercise {
         this.instructions = instructions;
     }
 
+    // Called prior to INSERT. Used to stamp both timestamps when the row is first created.
     @PrePersist
     void onCreate() {
         Instant now = Instant.now();
@@ -63,6 +72,7 @@ public class Exercise {
         this.updatedAt = now;
     }
 
+    // Called prior to UPDATE. Refreshed updatedAt. This solves a gap plain SQL "DEFAULT now()"
     @PreUpdate
     void onUpdate() {
         this.updatedAt = Instant.now();
