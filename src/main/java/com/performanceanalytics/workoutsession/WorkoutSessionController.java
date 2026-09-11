b862a61c4175daa0f56f0af705c4e16496afa796
fnilsson <fnilsson95@hotmail.com>
2026-09-11 20:21:11 +0200

    Create initial workoutsession skeleton

---
+++ src/main/java/com/performanceanalytics/workoutsession/WorkoutSessionController.java
@@ -0,0 +1,4 @@
+package com.performanceanalytics.workoutsession;
+
+public class WorkoutSessionController {
+}
---
+++ src/main/java/com/performanceanalytics/workoutsession/WorkoutSessionCreateRequest.java
@@ -0,0 +1,4 @@
+package com.performanceanalytics.workoutsession;
+
+public record WorkoutSessionCreateRequest() {
+}
