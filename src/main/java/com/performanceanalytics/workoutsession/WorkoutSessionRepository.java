d0480ac8d52325636746506d2add9aeb6c42c541
fnilsson <fnilsson95@hotmail.com>
2026-09-18 18:19:18 +0200

    Add WorkoutSession repository

--- src/main/java/com/performanceanalytics/workoutsession/WorkoutSessionRepository.java
+++ src/main/java/com/performanceanalytics/workoutsession/WorkoutSessionRepository.java
@@ -1,4 +1,18 @@
 package com.performanceanalytics.workoutsession;
 
-public interface WorkoutSessionRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+public interface WorkoutSessionRepository extends JpaRepository<WorkoutSession, Long> {
+
+    // Optional<T> --> a container that holds zero or one value.
+    // Use when the query implies at most one possible match, such as findByName where name is UNIQUE
+
+    // List<T> --> a container that holds zero, one, or many values.
+    // Use when the query allows multiple matches such as two sessions sharing a date.
+    List<WorkoutSession> findBySessionDate(LocalDate sessionDate);
 }
